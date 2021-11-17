f91ca68a7b90a434c416fe1afdbd0727ac49c1b2
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-17 18:18:08 +0100

    .

---
+++ js/list.js
@@ -0,0 +1,43 @@
+import { render, API } from "./utils.js";
+
+const List = (data) => {
+  const elements = data
+    .map(
+      (item) => `
+      <div class="scheda">
+
+        <img src="${item.poster}" alt="${item.title}">
+        <button class="delete" id="${item.id}"> x </button>
+        <a class="edit" href="#edit-${item.id}">✏️</a>
+        <p><strong>${item.title}<br></strong>${item.description}</p>
+      </div>`
+    )
+    .join("");
+
+  const container = document.querySelector("#container");
+  render(
+    container,
+    `
+    ${elements}
+    <a href="#add" id="add">Aggiungi una nuova scheda</a>
+    `
+  );
+
+  const btns = [...document.querySelectorAll(".delete")];
+  console.log(btns)
+  const deleteItem = (event) => {
+    console.log(event.target)
+    const id = parseInt(event.target.id);
+    const filtered = data.filter((movie) => movie.id !== id);
+
+    fetch(`${API}/${id}`, { method: "DELETE" })
+      .then((response) => response.json())
+      .then(() => List(filtered));
+  };
+
+  const btnClicks = (btn) => btn.addEventListener("click", deleteItem, { once: true });
+
+  btns.forEach(btnClicks);
+};
+
+export { List };
